6ee010fab62b302bf5d2a7ef84310b842d5616b2
irfanbayu <setiawan.irfanbayu@gmail.com>
2024-02-17 14:58:39 +0700

    add header, hero, browse, arrived, clients, aside, footer section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Browse from "./components/browse";
+import Arrived from "./components/arrived";
+import Clients from "./components/clients";
+import AsideMenu from "./components/asidemenu";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+    </>
   );
 }
 
