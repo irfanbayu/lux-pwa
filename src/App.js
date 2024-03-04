f6451fcb99e51c25a29eb5850145bc0ce472271d
irfanbayu <setiawan.irfanbayu@gmail.com>
2024-03-04 23:26:28 +0700

    cache json and image rest api

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App() {
   const [items, setItems] = React.useState([]);
   React.useEffect(function () {
     (async function () {
-      const response = await fetch("https://bwacharity.fly.dev/items");
+      const response = await fetch("https://bwacharity.fly.dev/items", {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      });
       const { nodes } = await response.json();
       setItems(nodes);
     })();
